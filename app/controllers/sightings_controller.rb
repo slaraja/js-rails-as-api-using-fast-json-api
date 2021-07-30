121f82272d4545061e29e6c9c574c347264e06bb
Stephanie La Raja <stephanielaraja@gmail.com>
2021-07-30 09:14:23 -0400

    commit tree 4c1137eeee5cb81092fde15f338f2c79a09f8099

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,24 @@
 class SightingsController < ApplicationController
+ 
+  def index
+    sightings = Sighting.all
+    render json: SightingSerializer.new(sightings)
+  end 
+  
+  # def show
+  #   sighting = Sighting.find(params[:id])
+  #   options = {
+  #     include: [:bird, :location]
+  #   render json: SightingSerializer.new(sighting)
+  # end
+
   def show
-    sighting = Sighting.find(params[:id])
-    render json: sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
-  end
+    sighting = Sighting.find_by(id: params[:id])
+    options = {
+      include: [:bird, :location]
+    }
+    render json: SightingSerializer.new(sighting, options)
+ #pass in a second options parameter to the serializer indicating that we want to include those objects
+  end 
+
 end
